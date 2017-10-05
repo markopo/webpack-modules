615efe68f5b209afceed3ad593a2588956d1cae3
Marko Poikkimäki <markopoikkimaki@Markos-MBP.lan>
2017-10-05 10:57:17 +0200

    Code splitting - System.import

--- src/index.js
+++ src/index.js
@@ -4,21 +4,24 @@
 
 
 
-import Mathematics from './sum';
-import ImageViewer from './image_viewer';
-
-
-const imageGallery = document.querySelector('#image-gallery');
-
-const iw = new ImageViewer();
-const image = iw.getImage();
-imageGallery.appendChild(image);
-
 
 document.getElementById('btn').addEventListener('click', (e) => {
-    const a = parseFloat(document.getElementById('a').value);
-    const b = parseFloat(document.getElementById('b').value);
-    document.getElementById('message').innerHTML = Mathematics.sum(a, b).toString();
+       e.preventDefault();
+
+
+       System.import('./image_viewer').then((module) => {
+
+           console.log(module);
+
+           const iw = new module.default();
+           const ig = document.querySelector('#image-gallery');
+           const img = iw.getImage();
+           ig.appendChild(img);
+
+       });
+
+
+
 });
 
 
